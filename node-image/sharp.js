f2a30332ca925705dfb8811b96be46c007a20ffd
lw <2212261902@qq.com>
2024-07-01 17:10:37 +0800

    feat:node-image

--- node-image/sharp.js
+++ node-image/sharp.js
@@ -1,11 +1,34 @@
 const sharp = require('sharp');
+const fs = require('fs');
+const ort = require('onnxruntime-node');
 let info = {
   className: '小鹿',
   point: [1959, 694, 2652, 1557], //x1 y1 x2 y2
   score: 0.98,
 };
+
 async function rectangle(img, item) {
-  // let data = await sharp(img).toBuffer();
+  // const session = await ort.InferenceSession.create('./squeezenet1_1.onnx');
+  // const { data, info } = await sharp(img)
+  //   .resize(224, 224) // 调整图像大小以匹配模型输入大小
+  //   .raw()
+  //   .toBuffer({ resolveWithObject: true });
+  // const { width, height, channels } = info;
+  // const uint8Array = new Uint8Array(data);
+  // console.log(width, height, channels);
+  // const float32Array = new Float32Array(uint8Array.length);
+  // for (let i = 0; i < uint8Array.length; i++) {
+  //   float32Array[i] = uint8Array[i];
+  // }
+  // const tensor = new ort.Tensor('float32', float32Array, [
+  //   1,
+  //   channels,
+  //   height,
+  //   width,
+  // ]);
+  // console.log(tensor);
+  // let outPut = await session.run({ data: tensor });
+  // console.log(outPut.cpuData);
   let data1 = await sharp(img)
     .composite([
       {
@@ -78,6 +101,7 @@ async function rectangle(img, item) {
         top: item.point[1] - 70,
       },
     ])
+
     .toFile('./combined.jpg');
   return data1;
 }
