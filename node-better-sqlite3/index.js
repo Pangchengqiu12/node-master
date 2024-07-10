3dcb38c49d3ef3bcd62a92629f21e63b2fdab2f3
lw <2212261902@qq.com>
2024-07-10 16:40:05 +0800

    feat:测试better-sqlite3

--- node-better-sqlite3/index.js
+++ node-better-sqlite3/index.js
@@ -1,17 +1,38 @@
 const sqlite3 = require('better-sqlite3');
 // 打开数据库
-const db = sqlite3('example.db');
+const DB = sqlite3('example.db');
 // 创建表
-db.prepare(
-  'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT,task TEXT)' //创建users表 如果存在就不创建
+DB.prepare(
+  'CREATE TABLE IF NOT EXISTS users(id TEXT PRIMARY KEY, name TEXT,task TEXT)' //创建users表 如果存在就不创建
 ).run();
-const insert = db.prepare('INSERT INTO users (id, name) VALUES (@id, @name)');
-const insertMany = db.transaction((users) => {
-  for (const user of users) insert.run(user);
+const insert = DB.prepare(
+  'INSERT INTO users (id, name,task) VALUES (@id, @name, @task)'
+);
+const find = DB.prepare('SELECT task FROM users WHERE id = ?');
+
+const insertMany = DB.transaction((user) => {
+  insert.run(user);
 });
-insertMany([
+let task = [
   {
-    id: '12312dsaf3',
-    name: '管理员2',
+    id: 'sjlkfgj',
+    taskName: '更新',
   },
-]);
+];
+console.log(
+  insertMany({
+    id: '12',
+    name: '管理员000',
+    task: JSON.stringify(task),
+  })
+);
+// function getUsers(id) {
+//   let info = find.get(id);
+//   console.log(info);
+//   return info;
+// }
+// getUsers('12312dsaf8');
+// const updateTask = DB.transaction((id, task) => {
+//   DB.prepare('UPDATE users SET task = ? WHERE id = ?').run(task, id);
+// });
+// updateTask('12312dsaf8', JSON.stringify(task));
