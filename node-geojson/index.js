9e558e8704ff7b9d2a75ceb3f16689de79adf289
lw <2212261902@qq.com>
2024-07-29 10:42:01 +0800

    优化坐标转换

--- node-geojson/index.js
+++ node-geojson/index.js
@@ -4,10 +4,12 @@
 const coordtransform = require('coordtransform');
 // const wgs84ToGcj02 = require('./utils');
 const fs = require('fs');
-
+const Path = require('path');
+const path = './langdao.geojson';
 // use an async context to call onnxruntime functions.
 async function main() {
-  let geojson = JSON.parse(fs.readFileSync('./protectedarea.geojson'));
+  let geojson = JSON.parse(fs.readFileSync(path));
+  const filename = Path.basename(path);
   // 执行坐标系转换
   geojson.features.forEach((feature) => {
     feature.geometry.coordinates.forEach((coordinate) => {
@@ -20,7 +22,11 @@ async function main() {
       });
     });
   });
-  fs.writeFileSync('protectedarea.json', JSON.stringify(geojson));
+  let outName = filename.split('.');
+  fs.writeFileSync(
+    filename.replace(outName[1], `_out.${outName[1]}`),
+    JSON.stringify(geojson)
+  );
 }
 
 main();
